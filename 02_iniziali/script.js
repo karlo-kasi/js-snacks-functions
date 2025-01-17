b68c2c0f4a654c51315afb255f195498033b986c
karlo-kasi <karlokasi1995@gmail.com>
2025-01-17 17:43:48 +0100

    Add:  folder 2 + folder 3

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+let primeLettere = []
 
 // Dichiara la funzione qui.
+function lettera (){
 
+    for (let i = 0; i < names.length; i++){
+      console.log (names[i][0])
+      primeLettere.push(names[i][0])
+
+}
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+lettera () 
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+console.log(primeLettere)
--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,27 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+function soloLettera (pippo, lettera){
+    let newArray = []
+    for (let i = 0; i < pippo.length; i++){
+        if (pippo[i][0].toUpperCase() === lettera.toUpperCase()){ 
+            newArray.push(pippo[i])
+        }
+    }
+         
+    if (newArray.length === 0){
+       console.log (`Non ci sono nomi che iniziano con ${lettera}`) 
 
+    } else {
+        return newArray
+    }   
+
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log (soloLettera(names, "A"))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
